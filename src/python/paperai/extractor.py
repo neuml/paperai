783cfb4b2663c1a8c8edeac2329a25fdec051630
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-08-04 16:54:47 -0400

    Add support for must clauses to custom query columns, closes #4

--- src/python/paperai/extractor.py
+++ src/python/paperai/extractor.py
@@ -62,12 +62,23 @@ class Extractor(object):
         # Build question-context pairs
         names, questions, contexts, snippets = [], [], [], []
         for name, query, question, snippet in queue:
+            # Get list of required tokens
+            must = [token.strip("+") for token in query.split() if token.startswith("+")]
+
+            # Tokenize search query
             query = Tokenizer.tokenize(query)
+
+            # List of matches
             matches = []
 
             scores = self.embeddings.similarity(query, tokenlist)
             for x, score in enumerate(scores):
-                matches.append(sections[x] + (score,))
+                # Get section text
+                text = sections[x][1]
+
+                # Add result if all required tokens are present or there are not required tokens
+                if not must or all([token.lower() in text.lower() for token in must]):
+                    matches.append(sections[x] + (score,))
 
             # Build context using top n best matching sections
             topn = sorted(matches, key=lambda x: x[2], reverse=True)[:3]
