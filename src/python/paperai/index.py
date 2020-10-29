5fb5fbb5758eab841e5614591db25bed1388e027
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-10-29 18:54:51 -0400

    Added parameter to limit number of documents to index

--- src/python/paperai/index.py
+++ src/python/paperai/index.py
@@ -23,12 +23,13 @@ class Index(object):
     SECTION_QUERY = "SELECT Id, Name, Text FROM sections WHERE (labels is null or labels NOT IN ('FRAGMENT', 'QUESTION'))"
 
     @staticmethod
-    def stream(dbfile):
+    def stream(dbfile, maxsize):
         """
         Streams documents from an articles.sqlite file. This method is a generator and will yield a row at time.
 
         Args:
             dbfile: input SQLite file
+            maxsize: maximum number of documents to process
         """
 
         # Connection to database file
@@ -36,7 +37,13 @@ class Index(object):
         cur = db.cursor()
 
         # Select tagged sentences without a NLP label. NLP labels are set for non-informative sentences.
-        cur.execute(Index.SECTION_QUERY + " AND tags is not null")
+        query = Index.SECTION_QUERY + " AND tags is not null"
+
+        if maxsize > 0:
+            query += "AND article in (SELECT id FROM articles ORDER BY entry DESC LIMIT %d)" % maxsize
+
+        # Run the query
+        cur.execute(query)
 
         count = 0
         for row in cur:
@@ -63,13 +70,14 @@ class Index(object):
         db.close()
 
     @staticmethod
-    def embeddings(dbfile, vectors):
+    def embeddings(dbfile, vectors, maxsize):
         """
         Builds a sentence embeddings index.
 
         Args:
             dbfile: input SQLite file
             vectors: vector path
+            maxsize: maximum number of documents to process
 
         Returns:
             embeddings index
@@ -82,21 +90,22 @@ class Index(object):
 
         # Build scoring index if scoring method provided
         if embeddings.config.get("scoring"):
-            embeddings.score(Index.stream(dbfile))
+            embeddings.score(Index.stream(dbfile, maxsize))
 
         # Build embeddings index
-        embeddings.index(Index.stream(dbfile))
+        embeddings.index(Index.stream(dbfile, maxsize))
 
         return embeddings
 
     @staticmethod
-    def run(path, vectors):
+    def run(path, vectors, maxsize):
         """
         Executes an index run.
 
         Args:
             path: model path, if None uses default path
             vectors: vector path, if None uses default path
+            maxsize: maximum number of documents to process
         """
 
         # Default path if not provided
@@ -110,8 +119,10 @@ class Index(object):
             vectors = Models.vectorPath("cord19-300d.magnitude")
 
         print("Building new model")
-        embeddings = Index.embeddings(dbfile, vectors)
+        embeddings = Index.embeddings(dbfile, vectors, maxsize)
         embeddings.save(path)
 
 if __name__ == "__main__":
-    Index.run(sys.argv[1] if len(sys.argv) > 1 else None, sys.argv[2] if len(sys.argv) > 2 else None)
+    Index.run(sys.argv[1] if len(sys.argv) > 1 else None,
+              sys.argv[2] if len(sys.argv) > 2 else None,
+              int(sys.argv[3]) if len(sys.argv) > 3 else 0)
