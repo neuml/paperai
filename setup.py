2c041319d7ac51e25af522dede478e9a88bde2de
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-12-09 09:24:52 -0500

    Bump version, add dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     DESCRIPTION = f.read()
 
 setup(name="paperai",
-      version="1.4.0",
+      version="1.5.0",
       author="NeuML",
       description="AI-powered literature discovery and review engine for medical/scientific papers",
       long_description=DESCRIPTION,
@@ -33,6 +33,7 @@ setup(name="paperai",
           "PyYAML>=5.3",
           "regex>=2020.5.14",
           "txtai>=1.4.0",
+          "txtmarker>=1.0.0"
       ],
       classifiers=[
           "License :: OSI Approved :: Apache Software License",
