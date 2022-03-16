83c56cb2a480511fe71a4517fc56f89a2098158e
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2022-03-16 16:12:37 -0400

    Debug test on windows

--- test/python/testquery.py
+++ test/python/testquery.py
@@ -29,9 +29,6 @@ class TestQuery(unittest.TestCase):
             with redirect_stdout(query):
                 Query.run("risk factors studied", 10, Utils.PATH)
 
-        with open(Utils.PATH + "/query.txt", "r", encoding="utf-8") as f:
-            print(f.read())
-
         self.assertEqual(
             Utils.hashfile(Utils.PATH + "/query.txt"),
             "c81b87b34b7efec05b79e61de319e6df",
