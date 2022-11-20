ff63ec258a37bd9042cacdc399003bf3db750ba2
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2022-11-20 08:35:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "txtai[api,similarity]>=4.3.1",
         "txtmarker>=1.0.0",
     ],
-    extra_requires=extras,
+    extras_requires=extras,
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
