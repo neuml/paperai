7323a43bf1570647eb34591450f18269bd7912d8
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2023-09-18 18:53:34 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extras["dev"] = ["black", "coverage", "coveralls", "httpx", "pre-commit", "pylin
 
 setup(
     name="paperai",
-    version="2.2.1",
+    version="2.3.0",
     author="NeuML",
     description="Semantic search and workflows for medical/scientific papers",
     long_description=DESCRIPTION,
